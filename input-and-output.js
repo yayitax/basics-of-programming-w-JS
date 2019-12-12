cd80dc527645fcd3afc0f3ca4195c011251be12c
Ximena <yayitax_cadena95@hotmail.com>
2019-12-12 15:55:44 -0700

    Initial commit: Finished module 1 - JS Introduction

---
+++ input-and-output.js
@@ -0,0 +1,13 @@
+/**
+ * Output
+ */
+//alert("Hello World!"); // Browser, not js alert
+
+console.log("Hello World!");
+console.log();
+
+/**
+ * Input
+ */
+//Not used often
+var age = prompt("What is your age?"); // Browser pop-up w input element
